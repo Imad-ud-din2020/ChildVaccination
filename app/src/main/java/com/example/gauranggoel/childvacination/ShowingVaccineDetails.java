547eedb9f55e36a9ce00334fadfa339e505dff5a
Gaurang Goel <gauranggoel.9@gmail.com>
2018-08-30 07:39:07 +0530

    working

--- app/src/main/java/com/example/gauranggoel/childvacination/ShowingVaccineDetails.java
+++ app/src/main/java/com/example/gauranggoel/childvacination/ShowingVaccineDetails.java
@@ -6,7 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +29,7 @@ import java.util.List;
 public class ShowingVaccineDetails extends Fragment {
 
 
+    public static final String TAG="showingVaccineDetailis";
     public ShowingVaccineDetails() {
         // Required empty public constructor
     }
@@ -40,6 +43,8 @@ AutoCompleteTextView autoCompleteTextView;
         // Inflate the layout for this fragment
 
         View v =inflater.inflate(R.layout.fragment_showing_vaccine_details, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+
         //getting items in arraylist so that it can be added further
         String name[]=getResources().getStringArray(R.array.vaccine_name);
         al=new ArrayList(Arrays.asList(name));
@@ -61,6 +66,7 @@ AutoCompleteTextView autoCompleteTextView;
                     if(s.equals(al.get(i)))
                         k=i;
                 }
+                Log.d(TAG,"inside autocomplete textview"+k);
 
                 callingFragment(k);
             }
@@ -85,6 +91,9 @@ AutoCompleteTextView autoCompleteTextView;
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 callingFragment(position);
+                Toast.makeText(getActivity().getApplicationContext(), ""+position, Toast.LENGTH_SHORT).show();
+
+                Log.d(TAG,"inside listview"+position);
             }
         });
 
@@ -95,11 +104,13 @@ AutoCompleteTextView autoCompleteTextView;
     void callingFragment(int k){
         VaccineDetailDescription obj = new VaccineDetailDescription();
         Bundle b = new Bundle();
-        b.putString("num",""+k);
+        b.putString("number",String.valueOf(k));
         obj.setArguments(b);
+        Log.d(TAG,"inside calling fragment");
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.vaccineDetails,new VaccineDetailDescription());
+        ft.replace(R.id.vaccineDetails,obj);
+        //ft.addToBackStack("VaccinDetailsDescription");
         ft.commit();
     }
 
--- app/src/main/java/com/example/gauranggoel/childvacination/VaccineDetailDescription.java
+++ app/src/main/java/com/example/gauranggoel/childvacination/VaccineDetailDescription.java
@@ -3,9 +3,22 @@ package com.example.gauranggoel.childvacination;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -14,6 +27,7 @@ import android.view.ViewGroup;
 public class VaccineDetailDescription extends Fragment {
 
 
+    public static final String TAG="VaccineDetailsDescription";
     public VaccineDetailDescription() {
         // Required empty public constructor
     }
@@ -23,7 +37,40 @@ public class VaccineDetailDescription extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vaccine_detail_description, container, false);
+
+        View v= inflater.inflate(R.layout.fragment_vaccine_detail_description, container, false);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
+        Bundle b = getArguments();
+
+        TextView name = v.findViewById(R.id.vaccineName);
+        TextView description = v.findViewById(R.id.vaccineDescription);
+
+        List al = new ArrayList(Arrays.asList(getResources().getStringArray(R.array.vaccine_name)));
+        List al1 = new ArrayList(Arrays.asList(getResources().getStringArray(R.array.vaccine_detail)));
+
+        //Toast.makeText(getActivity().getApplicationContext(), ""+getArguments(), Toast.LENGTH_SHORT).show();
+        String num=b.getString("number");
+        Log.d(TAG,"getting value"+num);
+        name.setText(""+al.get(Integer.parseInt(num)));
+        description.setText("" +al1.get(Integer.parseInt(num)));
+
+        ImageView img = v.findViewById(R.id.Vacinicon_icon);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.vaccineDetails,new ShowingVaccineDetails());
+                //ft.addToBackStack("VaccinDetailsDescription");
+                ft.commit();
+            }
+        });
+
+        return v;
     }
+
+
 
 }
