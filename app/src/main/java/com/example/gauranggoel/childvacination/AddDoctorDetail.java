b85e383fce2cbe26548ae44876acaec7bc1bbafe
Gaurang Goel <gauranggoel.9@gmail.com>
2018-08-31 20:17:42 +0530

    progress

--- app/src/main/java/com/example/gauranggoel/childvacination/AddDoctorDetail.java
+++ app/src/main/java/com/example/gauranggoel/childvacination/AddDoctorDetail.java
@@ -2,6 +2,8 @@ package com.example.gauranggoel.childvacination;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AddDoctorDetail extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class AddDoctorDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_doctor_detail);
+
+
+        ImageView img = (ImageView) findViewById(R.id.doc_icon);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
+
+
