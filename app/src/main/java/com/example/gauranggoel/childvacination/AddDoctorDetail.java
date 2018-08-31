f10d0c34f4a60f8c485730e42beffb7a2e67006b
Gaurang Goel <gauranggoel.9@gmail.com>
2018-08-31 20:13:43 +0530

    progress

---
+++ app/src/main/java/com/example/gauranggoel/childvacination/AddDoctorDetail.java
@@ -0,0 +1,13 @@
+package com.example.gauranggoel.childvacination;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDoctorDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_doctor_detail);
+    }
+}
